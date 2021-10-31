e43158cb0e7904c118f0347a584279044e8bf5c3
Patrick Mason <patrickm.mason@gmail.com>
2021-10-31 16:17:35 +1100

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
-
+const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
 app.listen(8000, function () {
   console.log("server is running");
+});
+
+app.get("/users", function (req, res) {
+  res.json({
+    success: true,
+    message: "successfully got users. Nice!",
+    user: mockUserData,
+  });
 });
